7cb64c6ff1d539d6a36caff97c64bec126d58b21
Alexander Trauzzi <atrauzzi@gmail.com>
2016-05-26 22:07:55 +0000

    Frozen!

---
+++ src/AuthAndApi/Repository/Authorization.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using AuthorizationEntity = AuthAndApi.Authorization;
+
+namespace AuthAndApi.Repository {
+
+    public interface Authorization<Identifier> {
+
+        AuthorizationEntity Get(Identifier id);
+
+        IEnumerable<AuthorizationEntity> GetForOwner(Owner owner);
+
+        IEnumerable<AuthorizationEntity> UpdateOrCreate();
+
+    }
+
+}
